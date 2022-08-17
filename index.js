2e7324dab0c0fb433027653fe347dbf0d9386d11
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-08-17 04:06:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+
+// when javascript is expecting a variable to contain one thing, and it does not that is known as AssertionError
+
+
+// string interpolation  using template literal
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
